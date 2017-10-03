3a7326262fd5f62ef7a94a774d129ad3843ed86c
Evans Mwendwa <evansmwendwa@users.noreply.github.com>
2017-10-03 22:08:17 +0300

    manual export of aot generated modules

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+export * from './lib/credit-card.validator';
+export * from './lib/directives/credit-card-format.directive';
+export * from './lib/directives/cvc-format.directive';
+export * from './lib/directives/expiry-format.directive';
+export * from './lib/shared/credit-card';
+export * from './lib/directives';
---
+++ index.js
@@ -0,0 +1,6 @@
+exports.CreditCardValidator = require('./lib/credit-card.validator').CreditCardValidator;
+exports.CreditCardFormatDirective = require('./lib/directives/credit-card-format.directive').CreditCardFormatDirective;
+exports.CvcFormatDirective = require('./lib/directives/cvc-format.directive').CvcFormatDirective;
+exports.ExpiryFormatDirective = require('./lib/directives/expiry-format.directive').ExpiryFormatDirective;
+exports.CreditCard = require('./lib/shared/credit-card').CreditCard;
+exports.CreditCardDirectivesModule = require('./lib/directives').CreditCardDirectivesModule;
