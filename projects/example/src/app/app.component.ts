fe0c63553c388cdc6b42ae70dd11d6859096d650
John Mckay <john@codebelfast.com>
2022-06-23 13:39:55 +0100

    update to angular 14

--- projects/example/src/app/app.component.ts
+++ projects/example/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Validators, FormGroup, FormBuilder } from '@angular/forms';
+import { Validators, UntypedFormGroup, UntypedFormBuilder } from '@angular/forms';
 import { CreditCardValidators, CreditCard } from 'angular-cc-library';
 import { defer } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -9,13 +9,13 @@ import { map } from 'rxjs/operators';
   templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
-  public demoForm: FormGroup;
+  public demoForm: UntypedFormGroup;
   public submitted = false;
 
   public type$ = defer(() => this.demoForm.get('creditCard').valueChanges)
     .pipe(map((num: string) => CreditCard.cardType(num)));
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: UntypedFormBuilder) {}
 
   public ngOnInit() {
     this.demoForm = this.fb.group({
@@ -31,7 +31,7 @@ export class AppComponent implements OnInit {
     }
   }
 
-  public onSubmit(demoForm: FormGroup) {
+  public onSubmit(demoForm: UntypedFormGroup) {
     this.submitted = true;
     console.log(demoForm.value);
   }
